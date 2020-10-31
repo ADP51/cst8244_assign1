2c5d9b95e4b724ed434230d371c39e351664ffa9
Karim Tahan <31939370+KarimTahan@users.noreply.github.com>
2020-10-30 23:57:45 -0400

    Update des_mva.h
    * Cleaned up outMessage array, added newline
    * Fixed up indentation

--- des_controller/src/des_mva.h
+++ des_controller/src/des_mva.h
@@ -29,30 +29,30 @@ typedef enum {
 
 #define NUM_INPUTS 11
 typedef enum {
-    LEFT_SCAN = 				0,
-    RIGHT_SCAN = 				1,
+    LEFT_SCAN = 			0,
+    RIGHT_SCAN = 			1,
     GUARD_LEFT_UNLOCK = 		2,
-    LEFT_OPEN = 				3,
-    LEFT_CLOSE = 				4,
+    LEFT_OPEN = 			3,
+    LEFT_CLOSE = 			4,
     GUARD_LEFT_LOCK = 			5,
-    WEIGHT_SCAN = 				6,
+    WEIGHT_SCAN = 			6,
     GUARD_RIGHT_UNLOCK = 		7,
-    RIGHT_OPEN = 				8,
-    RIGHT_CLOSE = 				9,
+    RIGHT_OPEN = 			8,
+    RIGHT_CLOSE = 			9,
     GUARD_RIGHT_LOCK = 			10
 } Input;
 
 #define NUM_OUTPUTS 13
 typedef enum {
-    START_MSG =					0,
-	READY_MSG = 				1,
-	LS_MSG =					2,
-	RS_MSG =					3,
+    	START_MSG =			0,
+	READY_MSG = 			1,
+	LS_MSG =			2,
+	RS_MSG =			3,
 	LEFTDOOR_UNLOCK_MSG =		4,
-	LEFTDOOR_OPEN_MSG =			5,
+	LEFTDOOR_OPEN_MSG =		5,
 	LEFTDOOR_CLOSED_MSG =		6,
 	LEFTDOOR_LOCKED_MSG = 		7,
-	WEIGHTSCAN_MSG =			8,
+	WEIGHTSCAN_MSG =		8,
 	RIGHTDOOR_UNLOCK_MSG = 		9,
 	RIGHTDOOR_OPEN_MSG =		10,
 	RIGHTDOOR_CLOSED_MSG =		11,
@@ -60,31 +60,30 @@ typedef enum {
 } Output;
 
 const char *outMessage[NUM_OUTPUTS] = {
-		"Controller PID: ",
-		"Ready for Entry",
-		"Person ID entered: ",
-		"Person ID entered: ",
-		"Left door unlocked",
-		"Left door opened",
-		"Left door closed",
-		"Left door locked",
-		"Weight scan",
-		"Right door unlocked",
-		"Right door opened",
-		"Right door closed",
-		"Right door locked"
+	"Controller PID:",
+	"Ready for Entry\n",
+	"Person ID entered:",
+	"Left door unlocked\n",
+	"Left door opened\n",
+	"Left door closed\n",
+	"Left door locked\n",
+	"Weight scan\n",
+	"Right door unlocked\n",
+	"Right door opened\n",
+	"Right door closed\n",
+	"Right door locked\n"
 };
 
 const char *inMessage[NUM_INPUTS] = {
-		"ls",
-		"rs",
-		"glu",
-		"lo",
-		"lc",
-		"gll",
-		"ws",
-		"gru",
-		"ro",
-		"rc",
-		"grl"
+	"ls",
+	"rs",
+	"glu",
+	"lo",
+	"lc",
+	"gll",
+	"ws",
+	"gru",
+	"ro",
+	"rc",
+	"grl"
 };
