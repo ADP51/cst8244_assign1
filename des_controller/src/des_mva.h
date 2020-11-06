5889d31f5b45213466065e6d610d4d854f4413e2
andrew <andrewpalmer51@gmail.com>
2020-11-06 14:27:07 -0500

    des_display complete

--- des_controller/src/des_mva.h
+++ des_controller/src/des_mva.h
@@ -3,94 +3,70 @@
 
 #define NUM_STATES 13
 typedef enum {
-    START_STATE =               	0,
-    WEIGHT_SCAN_STATE =   		1,
-    LEFT_SCAN_STATE = 			2,
-    RIGHT_SCAN_STATE = 			3,
-    LEFT_UNLOCK_STATE =   		4,
-    LEFT_OPEN_STATE =     		5,
-    LEFT_CLOSE_STATE =    		6,
-    LEFT_LOCK_STATE =      		7,
-    RIGHT_UNLOCK_STATE =  		8,
-    RIGHT_OPEN_STATE =    		9,
-    RIGHT_CLOSE_STATE =   		10,
-    RIGHT_LOCK_STATE =    		11,
-    EXIT = 				12
+	START_STATE = 0,
+	WEIGHT_SCAN_STATE = 1,
+	LEFT_SCAN_STATE = 2,
+	RIGHT_SCAN_STATE = 3,
+	LEFT_UNLOCK_STATE = 4,
+	LEFT_OPEN_STATE = 5,
+	LEFT_CLOSE_STATE = 6,
+	LEFT_LOCK_STATE = 7,
+	RIGHT_UNLOCK_STATE = 8,
+	RIGHT_OPEN_STATE = 9,
+	RIGHT_CLOSE_STATE = 10,
+	RIGHT_LOCK_STATE = 11,
+	EXIT = 12
 } State;
 
 #define NUM_INPUTS 11
 typedef enum {
-    LEFT_SCAN = 			0,
-    RIGHT_SCAN = 			1,
-    GUARD_LEFT_UNLOCK = 		2,
-    LEFT_OPEN = 			3,
-    LEFT_CLOSE = 			4,
-    GUARD_LEFT_LOCK = 			5,
-    WEIGHT_SCAN = 			6,
-    GUARD_RIGHT_UNLOCK = 		7,
-    RIGHT_OPEN = 			8,
-    RIGHT_CLOSE = 			9,
-    GUARD_RIGHT_LOCK = 			10
+	LEFT_SCAN = 0,
+	RIGHT_SCAN = 1,
+	GUARD_LEFT_UNLOCK = 2,
+	LEFT_OPEN = 3,
+	LEFT_CLOSE = 4,
+	GUARD_LEFT_LOCK = 5,
+	WEIGHT_SCAN = 6,
+	GUARD_RIGHT_UNLOCK = 7,
+	RIGHT_OPEN = 8,
+	RIGHT_CLOSE = 9,
+	GUARD_RIGHT_LOCK = 10
 } Input;
 
 #define NUM_OUTPUTS 13
 typedef enum {
-	READY_MSG = 			1,
-	LS_MSG =			2,
-	RS_MSG =			3,
-	LEFTDOOR_UNLOCK_MSG =		4,
-	LEFTDOOR_OPEN_MSG =		5,
-	LEFTDOOR_CLOSED_MSG =		6,
-	LEFTDOOR_LOCKED_MSG = 		7,
-	WEIGHTSCAN_MSG =		8,
-	RIGHTDOOR_UNLOCK_MSG = 		9,
-	RIGHTDOOR_OPEN_MSG =		10,
-	RIGHTDOOR_CLOSED_MSG =		11,
-	RIGHTDOOR_LOCKED_MSG = 		12,
+	READY_MSG = 1,
+	LS_MSG = 2,
+	RS_MSG = 3,
+	LEFTDOOR_UNLOCK_MSG = 4,
+	LEFTDOOR_OPEN_MSG = 5,
+	LEFTDOOR_CLOSED_MSG = 6,
+	LEFTDOOR_LOCKED_MSG = 7,
+	WEIGHTSCAN_MSG = 8,
+	RIGHTDOOR_UNLOCK_MSG = 9,
+	RIGHTDOOR_OPEN_MSG = 10,
+	RIGHTDOOR_CLOSED_MSG = 11,
+	RIGHTDOOR_LOCKED_MSG = 12,
 } Output;
 
-const char *outMessage[NUM_OUTPUTS] = {
-	"Controller PID:",
-	"Ready for Entry\n",
-	"Person ID entered:",
-	"Left door unlocked\n",
-	"Left door opened\n",
-	"Left door closed\n",
-	"Left door locked\n",
-	"Weight scan\n",
-	"Right door unlocked\n",
-	"Right door opened\n",
-	"Right door closed\n",
-	"Right door locked\n"
-};
+const char *outMessage[NUM_OUTPUTS] = { "Controller PID:", "Ready for Entry\n",
+		"Person ID entered:", "Person ID Leaving:", "Left door unlocked\n", "Left door opened\n",
+		"Left door closed\n", "Left door locked\n", "Weight scan\n",
+		"Right door unlocked\n", "Right door opened\n", "Right door closed\n",
+		"Right door locked\n" };
 
-const char *inMessage[NUM_INPUTS] = {
-	"ls",
-	"rs",
-	"glu",
-	"lo",
-	"lc",
-	"gll",
-	"ws",
-	"gru",
-	"ro",
-	"rc",
-	"grl"
-};
+const char *inMessage[NUM_INPUTS] = { "ls", "rs", "glu", "lo", "lc", "gll",
+		"ws", "gru", "ro", "rc", "grl" };
 
-
-struct response
-{
-	double answer;
+struct response {
 	int statusCode; // [OK, UNDEFINED, INVALID_OPERATOR, OVERFLOW]
 	char errorMsg[128];
-} typedef response_t;
+}typedef response_t;
 
-struct send
-{
+struct send {
 	char state;
 	int weight;
 	int person_id;
-} typedef send_t;
+}typedef send_t;
 
 #endif
--- des_display/src/des_display.c
+++ des_display/src/des_display.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../../des_controller/src/des_mva.h"
 
 int main(void) {
-	puts("Hello World!!!"); /* prints Hello World!!! */
+	send_t = msg_received;
+	resposne_t = response;
+	int chid = ChannelCreate(0);
+
+	if(chid < 0) {
+		return EXIT_FAILURE;
+	}
+
+	printf("Display's PID: %d\n", getpid());
+
+	while(1){
+//		Call MsgReceive() to receive Display object from controller
+		rcvid = MsgReceive(chid, &msg_received, size_of(send_t), NULL);
+
+//		Call MsgReply(), sending EOK back to the controller
+		MsgReply(rcvid, state, &response, size_of(response_t));
+
+//		IF message == ID_SCAN THEN
+		if(msg_received.state == LEFT_SCAN || msg_received.state == RIGHT_SCAN){
+			//Print person has been scanned entering (or leaving) the building and display the person's ID
+			if(msg_received.state == LEFT_SCAN){
+				printf("%s %d", outMessage[LEFT_SCAN], msg_received.person_id);
+			} else {
+				printf("%s %d", outMessage[RIGHT_SCAN], msg_received.person_id);
+			}
+		} else if(msg_received.state == WEIGHT_SCAN){ //ELSE IF message = WEIGHED THEN
+			//Print person has been weighed and their weight
+			printf("Person ID: %d has been weighed: %f.", msg_received.person_id, msg_received.weight);
+		} else {
+			//		Print message to Neutrino console (i.e. stdout)
+			printf("%s", outMessage[msg_received.state]);
+		}
+
+		if(msg_received.state == EXIT){ //IF message == EXIT THEN
+			break;
+		}
+
+	}
+
+	ChannelDestroy(chid);
 	return EXIT_SUCCESS;
+
 }
