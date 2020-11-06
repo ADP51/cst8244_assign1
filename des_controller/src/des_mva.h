aff58f6ec144be991239b15cbec97a9c93c13574
Karim Tahan <31939370+KarimTahan@users.noreply.github.com>
2020-11-05 20:20:50 -0500

    Update des_mva.h
    * Updated list of states
    * Added message structs (response to be fixed)

--- des_controller/src/des_mva.h
+++ des_controller/src/des_mva.h
@@ -1,58 +1,49 @@
 #ifndef DES_MVA_H_
 #define DES_MVA_H_
 
-#define NUM_STATES 22
+#define NUM_STATES 13
 typedef enum {
-    START_STATE =               0,
-    READY_STATE =               1,
-    LEFT_SCAN_STATE =           2,
-    RIGHT_SCAN_STATE =          3,
-    ENTER_LEFT_UNLOCK_STATE =   4,
-    ENTER_RIGHT_UNLOCK_STATE =  5,
-    ENTER_LEFT_OPEN_STATE =     6,
-    ENTER_RIGHT_OPEN_STATE =    7,
-    ENTER_WEIGHTSCAN_STATE =    8,
-    ENTER_LEFTCLOSE_STATE =     9,
-    ENTER_LEFTLOCK_STATE =      10,
-    ENTER_RIGHT_UNLOCK_STATE =  11,
-    ENTER_RIGHT_OPEN_STATE =    12,
-    ENTER_RIGHT_CLOSE_STATE =   13,
-    LEAVE_RIGHT_UNLOCK_STATE =  14,
-    LEAVE_RIGHT_OPEN_STATE =    15,
-    LEAVE_WEIGHTSCAN_STATE =    16,
-    LEAVE_RIGHT_CLOSE_STATE =   17,
-    LEAVE_RIGHTLOCK_STATE =     18,
-    LEAVE_LEFT_UNLOCK_STATE =   19,
-    LEAVE_LEFT_OPEN_STATE =     20,
-    LEAVE_LEFT_CLOSE_STATE =    21
+    START_STATE =               	0,
+    WEIGHT_SCAN_STATE =   		1,
+    LEFT_SCAN_STATE = 			2,
+    RIGHT_SCAN_STATE = 			3,
+    LEFT_UNLOCK_STATE =   		4,
+    LEFT_OPEN_STATE =     		5,
+    LEFT_CLOSE_STATE =    		6,
+    LEFT_LOCK_STATE =      		7,
+    RIGHT_UNLOCK_STATE =  		8,
+    RIGHT_OPEN_STATE =    		9,
+    RIGHT_CLOSE_STATE =   		10,
+    RIGHT_LOCK_STATE =    		11,
+    EXIT = 				12
 } State;
 
 #define NUM_INPUTS 11
 typedef enum {
-    LEFT_SCAN = 				0,
-    RIGHT_SCAN = 				1,
+    LEFT_SCAN = 			0,
+    RIGHT_SCAN = 			1,
     GUARD_LEFT_UNLOCK = 		2,
-    LEFT_OPEN = 				3,
-    LEFT_CLOSE = 				4,
+    LEFT_OPEN = 			3,
+    LEFT_CLOSE = 			4,
     GUARD_LEFT_LOCK = 			5,
-    WEIGHT_SCAN = 				6,
+    WEIGHT_SCAN = 			6,
     GUARD_RIGHT_UNLOCK = 		7,
-    RIGHT_OPEN = 				8,
-    RIGHT_CLOSE = 				9,
+    RIGHT_OPEN = 			8,
+    RIGHT_CLOSE = 			9,
     GUARD_RIGHT_LOCK = 			10
 } Input;
 
 #define NUM_OUTPUTS 13
 typedef enum {
-    START_MSG =					0,
-	READY_MSG = 				1,
-	LS_MSG =					2,
-	RS_MSG =					3,
+    START_MSG =				0,
+	READY_MSG = 			1,
+	LS_MSG =			2,
+	RS_MSG =			3,
 	LEFTDOOR_UNLOCK_MSG =		4,
-	LEFTDOOR_OPEN_MSG =			5,
+	LEFTDOOR_OPEN_MSG =		5,
 	LEFTDOOR_CLOSED_MSG =		6,
 	LEFTDOOR_LOCKED_MSG = 		7,
-	WEIGHTSCAN_MSG =			8,
+	WEIGHTSCAN_MSG =		8,
 	RIGHTDOOR_UNLOCK_MSG = 		9,
 	RIGHTDOOR_OPEN_MSG =		10,
 	RIGHTDOOR_CLOSED_MSG =		11,
@@ -62,7 +53,7 @@ typedef enum {
 const char *outMessage[NUM_OUTPUTS] = {
 		"Controller PID: ",
 		"Ready for Entry",
-		"Person ID entered: ",
+		"Enter Person ID:",
 		"Person ID entered: ",
 		"Left door unlocked",
 		"Left door opened",
@@ -88,3 +79,20 @@ const char *inMessage[NUM_INPUTS] = {
 		"rc",
 		"grl"
 };
+
+
+struct response
+{
+	double answer;
+	int statusCode; // [OK, UNDEFINED, INVALID_OPERATOR, OVERFLOW]
+	char errorMsg[128];
+} typedef response_t;
+
+struct send
+{
+	char state;
+	int weight;
+	int person_id;
+} typedef send_t;
+
+#endif
